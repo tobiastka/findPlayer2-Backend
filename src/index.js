05dfd8247abb10f1c2e3d436756857e4c687c7d1
Nai <62731284+tobiastka@users.noreply.github.com>
2023-05-24 06:57:22 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import express from 'express'
+import config from './config/index.js'
+import morgan from 'morgan'
+import sequelize from './db/index.js'
+import './db/models/User.js'
+import apiRounter from './routes/index.js'
 
 const app = express()
+app.use(morgan('tiny'))
+app.use(express.json())
 
-app.listen(3000, () => {
-  console.log('Servidor corriendo en el puerto 3000')
+app.get('/', (req, res) => {
+  res.status(200).send('API esta funcionando correctamente ^^')
+})
+app.use('/api', apiRounter)
+
+app.listen(config.app.port, async () => {
+  console.log(`Servidor corriendo en el puerto ${config.app.port}`)
+  await sequelize.sync({ force: false })
 })
